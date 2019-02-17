5575b25313dd37f3f28e7e1de5c79b60fde70ed6
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-02-17 15:19:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -176,11 +176,11 @@ function positionToInteger(p) {
 }
 
 function start() {
-  window.addEventListener('keydown', moveDodger)
+  window.addEventListener('keydown', moveDodger);
 
-  START.style.display = 'none'
+  START.style.display = 'none';
 
   gameInterval = setInterval(function() {
-    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
-  }, 1000)
+    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)));
+  }, 1000);
 }

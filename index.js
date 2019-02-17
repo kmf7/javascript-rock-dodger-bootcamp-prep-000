9c7c81bbcb23e6c997d8e124551ecbed22a29e35
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-02-17 15:29:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,22 +20,23 @@ var gameInterval = null;
 function checkCollision(rock) {
   // implement me!
   // use the comments below to guide you!
+
   const top = positionToInteger(rock.style.top);
 
   // rocks are 20px high
   // DODGER is 20px high
   // GAME_HEIGHT - 20 - 20 = 360px;
+
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left);
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = positionToInteger(DODGER.style.left)+40;
-  
+    const dodgerRightEdge = 0;
 
     const rockLeftEdge = positionToInteger(rock.style.left);
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = positionToInteger(rock.style.left) + 20;
+    const rockRightEdge = 0;
 
     if (false /**
                * Think about it -- what's happening here?
@@ -47,14 +48,17 @@ function checkCollision(rock) {
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
                */) {
-      if(((rockLeftEdge <= dodgerLeftEdge) && (rockRightEdge <= dodgerLeftEdge)) ||
-      ((rockLeftEdge >= dodgerLeftEdge) && (rockRightEdge <= dodgerRightEdge)) ||
-      ((rockLeftEdge <= dodgerRightEdge) &&(rockRightEdge >= dodgerRightEdge)))    {    
-      return true;}
+    const dodgerLeftEdge = positionToInteger(DODGER.style.left);
+    const dodgerRightEdge = positionToInteger(DODGER.style.left) + 40;
+    const rockLeftEdge = positionToInteger(rock.style.left);
+    const rockRightEdge = positionToInteger(rock.style.left) + 20;
+
+    if (((rockLeftEdge <= dodgerLeftEdge)&&(rockRightEdge >= dodgerLeftEdge)) || ((rockLeftEdge >= dodgerLeftEdge)&&(rockRightEdge <= dodgerRightEdge)) || ((rockLeftEdge <= dodgerRightEdge)&&(rockRightEdge >= dodgerRightEdge))) {
+      return true;
     }
   }
-}
-
+  return false;
+}}
 function createRock(x) {
   const rock = document.createElement('div');
   rock.className = 'rock';
